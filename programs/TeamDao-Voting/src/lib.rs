1f8215e94cbd6636b2094a41e263cb0472e727ef
cihancanayyildiz <cihancanayyildiz@gmail.com>
2022-10-05 17:37:51 +0300

    Create Team, Create Proposal, Invite Player, Join the Team functions implemented and tested with unit tests.

--- programs/TeamDao-Voting/src/lib.rs
+++ programs/TeamDao-Voting/src/lib.rs
@@ -5,11 +5,177 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod team_dao_voting {
     use super::*;
+    pub fn create_team(ctx: Context<CreateTeam>, name: String, player_capacity: u32) -> Result<()> {
+        let team = &mut ctx.accounts.team_account;
+        team.bump = *ctx.bumps.get("team_account").unwrap();
+        team.name = name;
+        team.player_capacity = player_capacity;
+        team.players = Vec::new();
+        team.invited_players = Vec::new();
+        team.team_captain = ctx.accounts.signer.key();
+        team.players.push(ctx.accounts.signer.key());
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Team {} successfully created. Owner of the team : {}", team.name, team.team_captain);
+        Ok(())
+    }
+
+    pub fn create_proposal(
+        ctx: Context<CreateProposal>,
+        title: String,
+        description: String
+    ) -> Result<()> {
+        let proposal = &mut ctx.accounts.proposal_account;
+        proposal.bump = *ctx.bumps.get("proposal_account").unwrap();
+        proposal.title = title;
+        proposal.description = description;
+        proposal.vote_yes = 0;
+        proposal.vote_no = 0;
+
+        /* 
+        let lmps: u64 = proposal.to_account_info().lamports();
+        proposal.lamports = lmps;*/
+
+        msg!(
+            "Proposal {} successfully created. Owner of the proposal: {}",
+            proposal.title,
+            ctx.accounts.signer.key()
+        );
+
+        Ok(())
+    }
+
+    pub fn invite_player(ctx: Context<InvitePlayer>, invited_player: Pubkey) -> Result<()> {
+        let team_account = &mut ctx.accounts.team_account;
+
+        if team_account.invited_players.len() >= team_account.player_capacity.try_into().unwrap() {
+            return err!(ErrorCode::TeamCapacityFullError);
+        }
+        if team_account.players.contains(&invited_player) {
+            return err!(ErrorCode::PlayerAlreadyExistsError);
+        }
+
+        team_account.invited_players.push(invited_player);
+
+        msg!("Player: {} invited to the team {}.", ctx.accounts.signer.key(), team_account.name);
+
+        Ok(())
+    }
+
+    pub fn join_the_team(ctx: Context<JoinTheTeam>) -> Result<()> {
+        let team_account = &mut ctx.accounts.team_account;
+
+        if team_account.players.len() >= team_account.player_capacity.try_into().unwrap() {
+            return err!(ErrorCode::TeamCapacityFullError);
+        }
+
+        if !team_account.invited_players.contains(&ctx.accounts.signer.key()) {
+            return err!(ErrorCode::PlayerIsNotInTheInvitedList);
+        }
+
+        team_account.players.push(ctx.accounts.signer.key());
+
+        msg!("Player: {} joined to the team {}.", ctx.accounts.signer.key(), team_account.name);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct CreateTeam<'info> {
+    #[account(
+        init,
+        payer = signer,
+        //todo: change space later
+        space = 1000,
+        seeds = ["team_account".as_bytes(), name.as_bytes()],
+        bump
+    )]
+    pub team_account: Account<'info, Team>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateProposal<'info> {
+    #[account(mut, seeds= ["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
+    pub team_account: Account<'info, Team>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 1000,
+        seeds = ["proposal_account".as_bytes(), title.as_bytes()],
+        bump,
+        constraint = team_account.team_captain == signer.key() // Only team captain can create proposal.
+    )]
+    pub proposal_account: Account<'info, Proposal>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InvitePlayer<'info> {
+    #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump, constraint = team_account.team_captain == signer.key())]
+    pub team_account: Account<'info, Team>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct JoinTheTeam<'info> {
+    #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
+    pub team_account: Account<'info, Team>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct LeaveTheTeam<'info> {
+    #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
+    pub team_account: Account<'info, Team>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Team {
+    pub name: String,
+    pub player_capacity: u32,
+    pub players: Vec<Pubkey>,
+    pub team_captain: Pubkey,
+    pub bump: u8,
+    pub invited_players: Vec<Pubkey>,
+}
+
+#[account]
+pub struct Proposal {
+    pub title: String,
+    pub owner: Pubkey,
+    pub description: String,
+    pub vote_yes: u64,
+    pub vote_no: u64,
+    pub bump: u8,
+    // pub lamports: u64,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Team capacity is full.")]
+    TeamCapacityFullError,
+    #[msg("Player already in the team.")]
+    PlayerAlreadyExistsError,
+    #[msg("Player is not in the invited list.")]
+    PlayerIsNotInTheInvitedList,
+}
--- tests/TeamDao-Voting.ts
+++ tests/TeamDao-Voting.ts
@@ -3,14 +3,176 @@ import { Program } from "@project-serum/anchor";
 import { TeamDaoVoting } from "../target/types/team_dao_voting";
 
 describe("TeamDao-Voting", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
 
-  const program = anchor.workspace.TeamDaoVoting as Program<TeamDaoVoting>;
+    anchor.setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const program = anchor.workspace.TeamDaoVoting as Program<TeamDaoVoting>;
+    const newPlayer = anchor.web3.Keypair.generate();
+    it("Create a Team.", async () => {
+        const [team] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode("team_account"),
+                anchor.utils.bytes.utf8.encode("Cihan's Team"),
+            ],
+            program.programId
+        );
+        console.log(`User1 key: ${provider.wallet.publicKey.toBase58()}`);
+        console.log(`Team1 acc: ${team.toBase58()}`);
+
+        const ix = program.methods.createTeam("Cihan's Team", 10).accounts({
+            teamAccount: team,
+        });
+        const tx = await ix.rpc().catch(console.error);
+        console.log(`Transaction id: ${tx}`);
+        const acc = (await ix.pubkeys()).teamAccount;
+        const data = await program.account.team.fetch(acc);
+        console.log(data.teamCaptain.toBase58());
+    });
+
+    it("Invite a player to the Team", async () => {
+        const [team] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode("team_account"),
+                anchor.utils.bytes.utf8.encode("Cihan's Team"),
+            ],
+            program.programId
+        );
+
+        console.log(`Invited player = ${newPlayer.publicKey}`);
+        const tx = await program.methods
+            .invitePlayer(newPlayer.publicKey)
+            .accounts({
+                teamAccount: team,
+            })
+            .rpc();
+
+        console.log(`Transaction id: ${tx}`);
+    });
+
+    it("Join the team", async () => {
+        const [team] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode("team_account"),
+                anchor.utils.bytes.utf8.encode("Cihan's Team"),
+            ],
+            program.programId
+        );
+
+        const tx = await program.methods
+            .joinTheTeam()
+            .accounts({
+                teamAccount: team,
+                signer: newPlayer.publicKey,
+            })
+            .signers([newPlayer])
+            .rpc();
+
+        console.log(`Transaction id: ${tx}`);
+
+        const accData = await program.account.team.fetch(team);
+        console.log(accData.players.map((player) => player.toBase58()));
+    });
+
+    xit("Create a proposal for team", async () => {
+        const [team] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode("team_account"),
+                anchor.utils.bytes.utf8.encode("Cihan's Team"),
+            ],
+            program.programId
+        );
+        const [proposal] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode("proposal_account"),
+                anchor.utils.bytes.utf8.encode("Cihan's Proposal"),
+            ],
+            program.programId
+        );
+
+        let proposal_user = anchor.web3.Keypair.generate();
+        let dropsig = await program.provider.connection.requestAirdrop(
+            proposal_user.publicKey,
+            anchor.web3.LAMPORTS_PER_SOL
+        );
+        await program.provider.connection.confirmTransaction(dropsig);
+
+        let dropsig2 = await program.provider.connection.requestAirdrop(
+            proposal,
+            5 * anchor.web3.LAMPORTS_PER_SOL
+        );
+
+        await program.provider.connection.confirmTransaction(dropsig2);
+
+        console.log(`Proposal user: ${proposal_user.publicKey}`);
+        console.log(`Proposal acc: ${proposal.toBase58()}`);
+
+        const ix = program.methods
+            .createProposal("Cihan's Proposal", "Voting")
+            .accounts({
+                teamAccount: team,
+                proposalAccount: proposal,
+            });
+
+        const tx = await ix.rpc().catch(console.error);
+
+        console.log(`Transaction: ${tx}`);
+
+        const proposals = await program.account.proposal.all();
+        const accs = await program.account.team.all();
+        console.log(proposals);
+        console.log(accs);
+        /*
+        const proposalacc = await program.account.proposal.fetch(proposal);
+        console.log(
+            proposalacc.lamports.toNumber() / anchor.web3.LAMPORTS_PER_SOL
+        );*/
+    });
+
+    xit("Create second Team.", async () => {
+        let user2 = anchor.web3.Keypair.generate();
+
+        let dropsig = await program.provider.connection.requestAirdrop(
+            user2.publicKey,
+            anchor.web3.LAMPORTS_PER_SOL
+        );
+
+        await program.provider.connection.confirmTransaction(dropsig);
+        const [team2] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                anchor.utils.bytes.utf8.encode("team_account"),
+                anchor.utils.bytes.utf8.encode("Can's Team"),
+            ],
+            program.programId
+        );
+        console.log(`User2 key: ${user2.publicKey.toBase58()}`);
+        console.log(`Team2 acc: ${team2.toBase58()}`);
+
+        const ix = program.methods
+            .createTeam("Can's Team", 10)
+            .accounts({
+                teamAccount: team2,
+                signer: user2.publicKey,
+            })
+            .signers([user2]);
+
+        const tx = await ix.rpc();
+        /*
+        const tx = await ix.transaction();
+        const transaction = new anchor.web3.Transaction().add(tx);
+        const signature = await provider.sendAndConfirm(transaction, [user2]);
+        console.log(signature);*/
+
+        console.log(`Transaction id: ${tx}`);
+        const acc = (await ix.pubkeys()).teamAccount;
+        const data = await program.account.team.fetch(acc);
+
+        const allAccs = await program.account.team.all();
+        console.log(allAccs);
+
+        allAccs.map((accs) => {
+            console.log(accs.account.teamCaptain.toBase58());
+        });
+    });
 });
