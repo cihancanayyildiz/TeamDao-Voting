3db3fd6b038f079ae8f6bcdecbe060c3bc644489
cihancanayyildiz <cihancanayyildiz@gmail.com>
2022-10-13 18:08:57 +0300

    Added comment lines.

--- programs/TeamDao-Voting/src/lib.rs
+++ programs/TeamDao-Voting/src/lib.rs
@@ -5,6 +5,8 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod team_dao_voting {
     use super::*;
+
+    // Creates a new team.
     pub fn create_team(ctx: Context<CreateTeam>, name: String, player_capacity: u32) -> Result<()> {
         let team = &mut ctx.accounts.team_account;
         team.bump = *ctx.bumps.get("team_account").unwrap();
@@ -20,6 +22,7 @@ pub mod team_dao_voting {
         Ok(())
     }
 
+    // Creates a new Proposal.
     pub fn create_proposal(
         ctx: Context<CreateProposal>,
         title: String,
@@ -46,6 +49,7 @@ pub mod team_dao_voting {
             }
             proposal.tournament_selection = tournament_selection.to_owned();
         } else if proposal.proposal_type == "Prize Distribution" {
+            //Tournament selection check
             if tournament_selection.is_empty() {
                 return err!(ErrorCode::TournamentSelectionIsInvalid);
             }
@@ -55,9 +59,11 @@ pub mod team_dao_voting {
                 return err!(ErrorCode::TournamentSelectionIsInvalid);
             }
 
+            // Prize distribution array check
             if prize_distribution.len() <= 0 || prize_distribution.len() != team.players.len() {
                 return err!(ErrorCode::PrizeDistributionParametersNotValid);
             }
+
             let prize_sum: u32 = prize_distribution.iter().sum();
             if prize_sum != 100 {
                 return err!(ErrorCode::PrizeDistributionParametersNotValid);
@@ -65,7 +71,7 @@ pub mod team_dao_voting {
             proposal.prize_distribution = prize_distribution;
             proposal.tournament_selection = tournament_selection.to_owned();
         } else {
-            // todo: throw an error.
+            return err!(ErrorCode::WrongProposalType);
         }
 
         msg!(
@@ -77,6 +83,7 @@ pub mod team_dao_voting {
         Ok(())
     }
 
+    // Invites a new player to team.
     pub fn invite_player(ctx: Context<InvitePlayer>, invited_player: Pubkey) -> Result<()> {
         let team_account = &mut ctx.accounts.team_account;
 
@@ -94,40 +101,45 @@ pub mod team_dao_voting {
         Ok(())
     }
 
+    // Invited player can join the team by using this function.
     pub fn join_the_team(ctx: Context<JoinTheTeam>) -> Result<()> {
         let team_account = &mut ctx.accounts.team_account;
 
+        // Capacity check
         if team_account.players.len() >= team_account.player_capacity.try_into().unwrap() {
             return err!(ErrorCode::TeamCapacityFullError);
         }
 
+        // Checks if player in invited players list or not.
         if !team_account.invited_players.contains(&ctx.accounts.signer.key()) {
             return err!(ErrorCode::PlayerIsNotInTheInvitedList);
         }
 
         team_account.players.push(ctx.accounts.signer.key());
 
-        //team_account.invited_players.retain(|&x| x == ctx.accounts.signer.key());
-
         msg!("Player: {} joined to the team {}.", ctx.accounts.signer.key(), team_account.name);
 
         Ok(())
     }
 
+    // Players can give vote to a proposal by using this function.
     pub fn give_vote(ctx: Context<GiveVote>, vote: String) -> Result<()> {
         let proposal = &mut ctx.accounts.proposal_account;
         let team = &mut ctx.accounts.team_account;
 
+        // Checking if proposal active or ended.
         if proposal.status != ProposalStatus::Active {
             return err!(ErrorCode::ProposalIsEnded);
         }
 
         let votes = vote.to_lowercase();
 
+        // Players cant vote more than one
         if proposal.voted_players.contains(&ctx.accounts.signer.key()) {
             return err!(ErrorCode::PlayerAlreadyVoted);
         }
 
+        // Vote type check.
         if votes == "yes" {
             proposal.vote_yes += 1;
         } else if votes == "no" {
@@ -138,6 +150,7 @@ pub mod team_dao_voting {
 
         proposal.voted_players.push(ctx.accounts.signer.key()); // pushing voter player to voted_players vector for validation check.
 
+        // If all players voted then proposal ends.
         if proposal.voted_players.len() == team.players.len() {
             if proposal.vote_yes > proposal.vote_no {
                 proposal.status = ProposalStatus::Accepted;
@@ -153,13 +166,16 @@ pub mod team_dao_voting {
         Ok(())
     }
 
+    // This function transfers the ownership of team to another player.
     pub fn transfer_ownership(ctx: Context<TransferOwnership>, new_captain: Pubkey) -> Result<()> {
         let team = &mut ctx.accounts.team_account;
 
+        // Old captain cant call this function.
         if team.team_captain == new_captain {
             return err!(ErrorCode::PlayerAlreadyTeamCaptain);
         }
 
+        // Checking if new captain in the team or not.
         if !team.players.contains(&new_captain) {
             return err!(ErrorCode::PlayerDoesntExist);
         }
@@ -171,6 +187,7 @@ pub mod team_dao_voting {
         Ok(())
     }
 
+    // Players can claim their tournament prize one by one by using this function.
     pub fn claim_the_prize(ctx: Context<ClaimPrize>, tournament_prize: u64) -> Result<()> {
         let proposal = &mut ctx.accounts.proposal_account;
         let team = &mut ctx.accounts.team_account;
@@ -184,22 +201,23 @@ pub mod team_dao_voting {
         {
             return err!(ErrorCode::ProposalIsInvalid);
         }
-        // Check lamports on the team account if it is >0 or not
-        //let team_lamports = 10_000_000_000;
 
         let team_lamports: u64 = tournament_prize;
+
+        // Valid prize check
         if team_lamports <= 0 {
             return err!(ErrorCode::TeamInsufficientFunds);
         }
-
+        // Checking if player in the team or not.
         if !team.players.contains(send_to_account.key) {
             return err!(ErrorCode::PlayerDoesntExist);
         }
-
+        // Players cant claim more than one.
         if proposal.claimed_players.contains(send_to_account.key) {
             return err!(ErrorCode::PlayerAlreadyClaimed);
         }
 
+        // Taking player index to find its prize distribution.
         let player_index = team.players
             .iter()
             .position(|player| player == send_to_account.key)
@@ -209,27 +227,18 @@ pub mod team_dao_voting {
         let prize_distribution = &proposal.prize_distribution;
         let player_prize = (team_lamports * (prize_distribution[player_index] as u64)) / 100;
 
-        // msg!("Balance: {}", pda_account.lamports());
-
+        // Transfering lamports from team account to player account.
         **pda_account.try_borrow_mut_lamports()? -= player_prize;
         **send_to_account.try_borrow_mut_lamports()? += player_prize;
 
+        // Pushing claimed players to claimed_players vector.
         proposal.claimed_players.push(send_to_account.key.to_owned());
-
-        /* 
-        let seeds = &["team_account".as_bytes(), team.name.as_bytes(), &[team.bump]];
-        let ix = transfer(&team.key(), &ctx.accounts.signer.key(), lamports[0]);
-
-        invoke_signed(
-            &ix,
-            &[team.to_account_info(), ctx.accounts.signer.to_account_info()],
-            &[&seeds[..]]
-        )?;*/
 
         Ok(())
     }
 }
 
+// Creates a new team.
 #[derive(Accounts)]
 #[instruction(name: String)]
 pub struct CreateTeam<'info> {
@@ -247,6 +256,7 @@ pub struct CreateTeam<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Create proposal struct that creates proposal and holds team account inside it.
 #[derive(Accounts)]
 #[instruction(title: String)]
 pub struct CreateProposal<'info> {
@@ -269,6 +279,7 @@ pub struct CreateProposal<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Give vote struct that holds team account and proposal account.
 #[derive(Accounts)]
 pub struct GiveVote<'info> {
     #[account(mut, seeds= ["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
@@ -283,6 +294,7 @@ pub struct GiveVote<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Invite Player struct that allows program to invite players to team.
 #[derive(Accounts)]
 pub struct InvitePlayer<'info> {
     #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump, constraint = team_account.team_captain == signer.key())]
@@ -293,6 +305,7 @@ pub struct InvitePlayer<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Join team struct that takes team account as parameter and allows users to join the team.
 #[derive(Accounts)]
 pub struct JoinTheTeam<'info> {
     #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
@@ -303,6 +316,7 @@ pub struct JoinTheTeam<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Leave team struct that takes team account as parameter and allows users to leave the team.
 #[derive(Accounts)]
 pub struct LeaveTheTeam<'info> {
     #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
@@ -313,6 +327,7 @@ pub struct LeaveTheTeam<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// TransferOwnership struct that takes team as parameter.
 #[derive(Accounts)]
 pub struct TransferOwnership<'info> {
     #[account(mut, seeds=["team_account".as_bytes(), team_account.name.as_bytes()],bump, constraint = team_account.team_captain == signer.key())] // Only team captain can change the ownership.
@@ -323,6 +338,7 @@ pub struct TransferOwnership<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Claim Prize struct that takes team account and proposal account as parameter
 #[derive(Accounts)]
 pub struct ClaimPrize<'info> {
     #[account(mut,seeds=["team_account".as_bytes(), team_account.name.as_bytes()], bump)]
@@ -336,6 +352,7 @@ pub struct ClaimPrize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+// Team  account that holds information about team
 #[account]
 pub struct Team {
     pub name: String,
@@ -347,6 +364,7 @@ pub struct Team {
     pub tournaments: Vec<String>,
 }
 
+// Proposal account that holds information about Proposal
 #[account]
 pub struct Proposal {
     pub title: String,
@@ -363,6 +381,7 @@ pub struct Proposal {
     pub claimed_players: Vec<Pubkey>,
 }
 
+// Proposal Status Enum
 #[derive(Debug, Clone, PartialEq, AnchorSerialize, AnchorDeserialize)]
 pub enum ProposalStatus {
     Active,
@@ -371,6 +390,7 @@ pub enum ProposalStatus {
     Draw,
 }
 
+// Error handling enums
 #[error_code]
 pub enum ErrorCode {
     #[msg("Team capacity is full.")]
@@ -401,4 +421,6 @@ pub enum ErrorCode {
     TeamInsufficientFunds,
     #[msg("Player already claimed his prize.")]
     PlayerAlreadyClaimed,
+    #[msg("Please enter proper proposal type!")]
+    WrongProposalType,
 }
